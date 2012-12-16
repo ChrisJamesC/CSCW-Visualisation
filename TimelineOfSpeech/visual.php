8a2172c5a07eeae1dfe25f3f4159bbc6dc5d8d61
Etchira <thomas.droxler@gmail.com>
2012-12-16 16:18:11 +0100

    add last files

---
+++ TimelineOfSpeech/visual.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Time line of speech</title>
+  </head>
+ <body>
+  </body>
+
+  <?php $file = $_GET["file"];?>
+  <h1><a href="circle.php?file=<?php echo $file?>">Circle</a>
+  <h1><a href="circle2.php?file=<?php echo $file?>">Circle2</a>
+  <h1><a href="rect.php?file=<?php echo $file?>">Rectangle</a>
+
+</html>
