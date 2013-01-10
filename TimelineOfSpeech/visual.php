7d2651c66f6a2eabe2544ad4a5c63bb7b1d4ab45
Etchira <thomas.droxler@gmail.com>
2013-01-10 09:26:19 +0100

    add Parrot-visualisation folder

--- TimelineOfSpeech/visual.php
+++ TimelineOfSpeech/visual.php
@@ -10,4 +10,5 @@
 	<h1><a href="circle.php?file=<?php echo $file?>">Ellipse</a>
 	<h1><a href="aoe.php?file=<?php echo $file?>">Area</a>
 	<h1><a href="both.php?file=<?php echo $file?>">Both</a>
+	<h1><a href="aoeNvd3.php?file=<?php echo $file?>">AreaNVD3</a>
 </html>
--- TimelineOfSpeech/visualBeaver.php
+++ TimelineOfSpeech/visualBeaver.php
@@ -10,4 +10,5 @@
 	<h1><a href="circle.php?file=<?php echo $file?>">Ellipse</a>
 	<h1><a href="aoeBeaver.php?file=<?php echo $file?>">Area</a>
 	<h1><a href="bothBeaver.php?file=<?php echo $file?>">Both</a>
+	<h1><a href="aoeBeaverNvd3.php?file=<?php echo $file?>">AreaNVD3</a>
 </html>
